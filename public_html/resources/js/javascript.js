b6a5dda075e18aa037924623a21629fd8e52c612
Holmes Marin <holmarin@uan.edu.co>
2018-11-05 10:46:53 -0500

    Version inicial Sep 22 2018

---
+++ public_html/resources/js/javascript.js
@@ -0,0 +1,10 @@
+/* 
+    Created on : 21/09/2018, 12:06:24 AM
+    Author     : Holmes Marin
+    Title      : UAN Tesis project
+*/
+$(document).ready(function() {
+    $('#lanzar_alerta').click(function() {
+        alert('hola mundo!');
+    });
+});
